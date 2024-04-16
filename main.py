8c7c37f8da557652b1180d67ecbca8c8f82fa265
MohammadHosseinHeidary <mohammadhsein9@gmail.com>
2024-04-16 12:12:18 +0330

    final

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import logging
 from log import log
 from fastapi.middleware.cors import CORSMiddleware
 from cache.cache_manager import CacheManager
+from rtdb.rethink import RethinkDBConnection
 import json
 
 app = FastAPI()
@@ -26,9 +27,21 @@ logger = logging.getLogger(__name__)
 
 @app.post("/gps/")
 def record_location(gps: dict):
+    latitude = gps['data']['latitude']
+    longitude = gps['data']['longitude']
+
+    # Insert the extracted data into RethinkDB
+    document = {"plate": gps['plate'], "location": [longitude, latitude],
+                "driver_id": gps['data']['driver_id']}
     cache = CacheManager()
 
+    rt = RethinkDBConnection()
+
     res = cache.hset(str(gps['plate']), gps['data'])
+    rt.connect()
+    # rt.create_table('geo_data')
+    rt.insert_document(table_name='geo_data', document=document)
+    rt.close()
 
     return res
 
@@ -40,6 +53,29 @@ def read_location(plate: str):
     res = cache.hget(plate)
 
     return res
+
+
+@app.get("/gps_track/{plate}")
+def track_location(plate: str):
+    print(plate)
+    rt = RethinkDBConnection()
+    rt.connect()
+    docs = rt.get_documents_by_key(table_name='people', key='plate', value=plate)
+
+    return docs
+
+
+@app.post("/geo_query/")
+def geo_query(location: dict):
+    print(location)
+    rt = RethinkDBConnection()
+    rt.connect()
+    rt.create_geo_index('geo_data', 'location')
+
+    radius_in_km = 10  # Adjust the radius as needed
+    documents = rt.get_documents_by_geo('geo_data', 'location', 98756875986, 987765424, 10)
+
+    return documents
 
 
 log.setup_logger()
